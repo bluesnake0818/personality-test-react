990bf08e2695206732252b42a3e605b59a966093
bluesnake0818 <raidient87@gmail.com>
2022-04-12 09:17:04 -0400

    stub up landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,22 +3,24 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <div className={styles.card}>
-        <div className={styles.titleArea}>
-          <h1 className={styles.fullTitle}>hangfive Personality Test</h1>
-          <h1 className={styles.shortTitle}>Personality Test</h1>
-          <h3 className={styles.subtitle}>How do I react when I discover I have a crush?</h3>
+      {/* <div className={styles.bodyContent}> */}
+        <div className={styles.card}>
+          <div className={styles.titleArea}>
+            <h1 className={styles.fullTitle}>hangfive Personality Test</h1>
+            <h1 className={styles.shortTitle}>Personality Test</h1>
+            <h3 className={styles.subtitle}>How do I react when I discover I have a crush?</h3>
+          </div>
+          <img className={styles.image} src="/rabbit_3d.png" alt="bunny"/>
+          <button className={styles.btn}>Start Test</button>
         </div>
-        <img className={styles.image} src="/rabbit_3d.png" alt="bunny"/>
-        <button className={styles.btn}>Start Test</button>
-      </div>
-        <div className={styles.imageArea}>
-          <img className={styles.imageBig} src="/rabbit_3d.png" alt="bunny"/>
+          <div className={styles.imageArea}>
+            <img className={styles.imageBig} src="/rabbit_3d.png" alt="bunny"/>
+          </div>
+        <div className={styles.footer}>
+          <p>Copyright 2022 HangFive, Inc.</p>
+          <p>All rights reserved.</p>
         </div>
-      <div className={styles.footer}>
-        <p>Copyright 2022 HangFive, Inc.</p>
-        <p>All rights reserved.</p>
-      </div>
+      {/* </div> */}
       <div className={styles.footerInLine}>
         <p>Copyright 2022 HangFive, Inc. All rights reserved.</p>
       </div>
