84c0f35c51132fc898d03b427a29f7d4c8863b85
bluesnake0818 <raidient87@gmail.com>
2022-04-12 09:00:34 -0400

    stub up /landing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,52 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
+// import PersonIcon from '@mui/icons-material/Person'
+// import ExtensionIcon from '@mui/icons-material/Extension';
+// import LogoutIcon from '@mui/icons-material/Logout';
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
-      {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          </ul>
-        </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
-    </>
+    <main className={styles.container}>
+      <Link to="/" className={styles.title}>
+        <p >hangfive</p>
+      </Link>
+      <div className={styles.profile}>
+        {user ?
+          <nav>
+            <ul className={styles.userNavLinks}>
+              <li className={styles.listItem}>
+                <Link to="" className={styles.link} onClick={handleLogout}>
+                  <p className={styles.paragraph}>Log Out</p>
+                </Link>
+              </li>
+              <li className={styles.listItem}>
+                <Link to="/profile" className={styles.link} >
+                  <p className={styles.paragraph}>{user.name}'s Profile</p>
+                </Link>
+              </li>
+              <li className={styles.listItem}>
+                <Link to="/compat" className={styles.link} >
+                  <p className={styles.paragraph}>Compatibility</p>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        :
+          <nav>
+            <ul className={styles.guestNavLinks}>
+              <li className={styles.listItem}>
+                <Link className={styles.link} to="/login">Log In</Link>
+              </li>
+              <li className={styles.listItem}>
+                <Link className={styles.link} to="/signup">Sign Up</Link>
+              </li>
+            </ul>
+          </nav>
+        } 
+      </div>
+    </main>
   )
 }
 
 export default NavBar
+
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,25 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <div className={styles.card}>
+        <div className={styles.titleArea}>
+          <h1 className={styles.fullTitle}>hangfive Personality Test</h1>
+          <h1 className={styles.shortTitle}>Personality Test</h1>
+          <h3 className={styles.subtitle}>How do I react when I discover I have a crush?</h3>
+        </div>
+        <img className={styles.image} src="/rabbit_3d.png" alt="bunny"/>
+        <button className={styles.btn}>Start Test</button>
+      </div>
+        <div className={styles.imageArea}>
+          <img className={styles.imageBig} src="/rabbit_3d.png" alt="bunny"/>
+        </div>
+      <div className={styles.footer}>
+        <p>Copyright 2022 HangFive, Inc.</p>
+        <p>All rights reserved.</p>
+      </div>
+      <div className={styles.footerInLine}>
+        <p>Copyright 2022 HangFive, Inc. All rights reserved.</p>
+      </div>
     </main>
   )
 }
